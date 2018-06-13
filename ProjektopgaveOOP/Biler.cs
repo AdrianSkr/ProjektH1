7ba651bee83090b0a25050074554335a0a75eae6
Adrian Borgen <exponentialname@outlook.com>
2018-06-13 09:48:14 +0200

    class changes

--- ProjektopgaveOOP/Biler.cs
+++ ProjektopgaveOOP/Biler.cs
@@ -1,39 +1,39 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace ProjektopgaveOOP
-//{
-//    public class Biler:Værksted
-//    {
-//        public string regNr { get; set; }
-//        public string Mærke { get; set; }
-//        public string modelÅrgang { get; set; }
-//        public string Kilometer { get; set; }
-//        public string Brændstof { get; set; }
-//        public string Interior { get; set; }
+namespace ProjektopgaveOOP
+{
+    public class Biler:Værksted
+    {
+        public string regNr { get; set; }
+        public string Mærke { get; set; }
+        public string modelÅrgang { get; set; }
+        public string Kilometer { get; set; }
+        public string Brændstof { get; set; }
+        public string Interior { get; set; }
 
-//        //default constructor
-//        public Biler(string opretDato, string flereBiler) : base(opretDato)
-//        {
-//            this.Biler = flereBiler;
-//        }
+        //default constructor
+        public Biler(string opretDato, string Nummerplade, string Mærke, string modelÅrgang) : base(opretDato)
+        {
+            this.regNr = Nummerplade;
+        }
 
-//        public Biler(string bilEjerTypeE)
-//        {
-//            this.Erhvev = bilEjerTypeE;
-//        }
+        public Biler(string Mærke)
+        {
+            this.Mærke = Mærke;
+        }
 
-//        public Biler(string bilEjerTypeP, string opretDato, string flereBiler)
-//        {
-//            this.Privat = bilEjerTypeP;
-//        }
+        public Biler(string modelÅrgang, string Mærke)
+        {
+            this.modelÅrgang = modelÅrgang;
+        }
 
-//        //Overrider virtual method in base class værksted, som hedder KundeInformation
-//        public override string KundeInformation()
-//        {
-//            return base.KundeInformation() + (" " + Biler + " " + Oprettelsesdato + " ");
-//        }
-//    }
-//}
+        //Overrider virtual method in base class værksted, som hedder KundeInformation
+        public override string KundeInformation()
+        {
+            return base.KundeInformation() + (" " + regNr + " " + Oprettelsesdato + " ");
+        }
+    }
+}
--- ProjektopgaveOOP/Tires.cs
+++ ProjektopgaveOOP/Tires.cs
@@ -1,32 +1,32 @@
-﻿//using System;
-//namespace ProjektopgaveOOP
-//{
-//    public class Tires:Biler
-//    {
-//        public string Brand { get; set; }
-//        public string tireSize { get; set; }
-//        public string tireType { get; set; }
+﻿using System;
+namespace ProjektopgaveOOP
+{
+    public class Tires:Biler
+    {
+        public string Brand { get; set; }
+        public string tireSize { get; set; }
+        public string tireType { get; set; }
 
-//        //default constructor
-//        public Tires(string opretDato, string flereBiler) : base(opretDato)
-//        {
-//            this.Brand = flereBiler;
-//        }
+        //default constructor
+        public Tires(string opretDato, string flereBiler, string tireBrand) : base(opretDato)
+        {
+            this.Brand = tireBrand;
+        }
 
-//        public Biler(string bilEjerTypeE)
-//        {
-//            this.Erhvev = bilEjerTypeE;
-//        }
+        public Tires(string Size)
+        {
+            this.tireSize = Size;
+        }
 
-//        public Biler(string bilEjerTypeP, string opretDato, string flereBiler)
-//        {
-//            this.Privat = bilEjerTypeP;
-//        }
+        public Biler(string bilEjerTypeP, string opretDato, string flereBiler)
+        {
+            this.Privat = bilEjerTypeP;
+        }
 
-//        //Overrider virtual method in base class værksted, som hedder KundeInformation
-//        public override string KundeInformation()
-//        {
-//            return base.KundeInformation() + (" " + Biler + " " + Oprettelsesdato + " ");
-//        }
-//    }
-//}
+        //Overrider virtual method in base class værksted, som hedder KundeInformation
+        public override string KundeInformation()
+        {
+            return base.KundeInformation() + (" " + Brand + " " + Oprettelsesdato + " ");
+        }
+    }
+}
